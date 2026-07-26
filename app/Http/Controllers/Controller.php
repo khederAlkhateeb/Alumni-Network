8d32481b6c1d0982237fa1fd39960669a32b05b8
Haedara Deeb <haedaraedeeb@gmail.com>
2026-07-26 16:17:51 +0300

    add the base structure

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+
 abstract class Controller
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * Return a success API response.
+     */
+    protected function successResponse(mixed $data = null, ?string $message = null, array $meta = [], int $code = 200): JsonResponse
+    {
+        return response()->json([
+            'status'  => 'success',
+            'message' => $message,
+            'data'    => $data,
+            'meta'    => $meta,
+        ], $code);
+    }
+
+    /**
+     * Return an error API response.
+     */
+    protected function errorResponse(string $message, array $errors = [], int $code = 400): JsonResponse
+    {
+        return response()->json([
+            'status'  => 'error',
+            'message' => $message,
+            'errors'  => $errors,
+        ], $code);
+    }
 }
