5912be1cf8ecb50d671294171d6b3a30a6832ec8
qusay_kd <kosaykm71@gmail.com>
2026-08-03 22:25:06 +0300

    Refactor ConnectionResource to conditionally include sender and receiver based on user ID; and updated ServiceProvider

--- app/Http/Resources/ConnectionResource.php
+++ app/Http/Resources/ConnectionResource.php
@@ -17,11 +17,20 @@ class ConnectionResource extends JsonResource
         return [
             'id' => $this->id,
             'status' => $this->status,
-            'receiver' => $this->whenLoaded('receiver', fn() => [
-                'id' => $this->receiver->id,
-                'name' => $this->receiver->name,
-                'role' => $this->receiver->role_label,
-            ], []),
+            $this->mergeWhen($this->sender->id != $request->user()->id, [
+                'sender' => $this->whenLoaded('sender', fn() => [
+                    'id' => $this->sender->id,
+                    'name' => $this->sender->name,
+                    'role' => $this->sender->role_label,
+                ], []),
+            ]),
+             $this->mergeWhen($this->receiver->id != $request->user()->id, [
+                'receiver' => $this->whenLoaded('receiver', fn() => [
+                    'id' => $this->receiver->id,
+                    'name' => $this->receiver->name,
+                    'role' => $this->receiver->role_label,
+                ], []),
+            ]),
             'created_at' => $this->created_at?->toIso8601String(),
         ];
     }
